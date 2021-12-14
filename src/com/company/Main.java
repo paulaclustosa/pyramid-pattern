de57c35207b8f20862f907c9b1ff94e3281c1024
Paula Lustosa <86850247+paulaclustosa@users.noreply.github.com>
2021-12-14 17:47:14 -0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        boolean isRunning = true;
+        boolean isRunning;
         Scanner inputScan = new Scanner(System.in);
 
         printWelcomeMessage();

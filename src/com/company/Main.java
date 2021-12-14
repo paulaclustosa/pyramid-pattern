51043eca5841535010ed6d5785cd5dd32b92a57d
Paula Lustosa <pl.lustosa@gmail.com>
2021-12-14 12:59:54 -0300

    Added continuity option

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,57 @@
 package com.company;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int userInputNumber = readUserInputNumber();
-        String [][] pyramid = createPyramid(userInputNumber);
-        printPyramid(pyramid);
+        printWelcomeMessage();
+        boolean isRunning = true;
+        Scanner inputScan = new Scanner(System.in);
+
+        do {
+            int userInputNumber = readUserInputNumber(inputScan);
+            String [][] pyramid = createPyramid(userInputNumber);
+            printPyramid(pyramid);
+            isRunning = setProgramContinuity(inputScan);
+        } while (isRunning);
+
     }
 
-    public static final String WELCOME_MESSAGE = "\n----- " +
+    static final String WELCOME_MESSAGE = "----- " +
             "Welcome to the Pyramid Pattern Program!" +
-            " -----\n" +
+            " -----";
+
+    static final String ADD_NUMBER = "\nPlease add an integer number: ";
+
+    static final String CONTINUITY_MESSAGE = "Cool, right? =)\n" +
+            "Do you want to see another pyramid with a different entry number? " +
             "\n" +
-            "Please add an integer number: ";
+            "Enter Y for yes or N for no: ";
 
     public static void printWelcomeMessage() {
-        System.out.print(WELCOME_MESSAGE);
+        System.out.println(WELCOME_MESSAGE);
     }
 
-    public static int readUserInputNumber() {
-        Scanner inputScan = new Scanner(System.in);
-        printWelcomeMessage();
+    public static void printAddNumberMessage() {
+        System.out.print(ADD_NUMBER);
+    }
+
+    public static void printContinuityMessage() {
+        System.out.println(CONTINUITY_MESSAGE);
+    }
+
+    public static int readUserInputNumber(Scanner inputScan) {
+        printAddNumberMessage();
         return inputScan.nextInt();
     }
 
     public static String[][] createPyramid(int userInputNumber) {
-        final int ADJUST_IN_LINE_LENGTH = userInputNumber + 2;
+        int LINE_LENGTH = userInputNumber;
+        int ADJUST_IN_COLUMN_LENGTH = userInputNumber + 2;
 
-        String[][] pyramid = new String[userInputNumber][ADJUST_IN_LINE_LENGTH];
+        String[][] pyramid = new String[LINE_LENGTH][ADJUST_IN_COLUMN_LENGTH];
 
         for (int i = 0; i < pyramid.length; i++) {
             for (int j = pyramid[i].length - 1; j >= pyramid[i].length - 1 - i; j--) {
@@ -43,12 +65,19 @@ public class Main {
     }
 
     public static void printPyramid(String[][] pyramid) {
+        System.out.print('\n');
         for (int i = 0; i < pyramid.length; i++) {
             for (int j = 0; j < pyramid[i].length; j++) {
                 System.out.print(pyramid[i][j]);
             }
             System.out.print('\n');
         }
+        System.out.print('\n');
+    }
+
+    public static boolean setProgramContinuity(Scanner inputScan) {
+        printContinuityMessage();
+        return Objects.equals(inputScan.next(), "Y");
     }
 
 }
